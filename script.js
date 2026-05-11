33cc766c800f164986da2d1e24ff11199989bdf1
Iryna Glytsko <82415832+IrynaGlytsko@users.noreply.github.com>
2026-05-11 16:33:22 +0300

    New-branch-4 (#6)
    * Верстка макета
    
    * Верстка по макету

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Находим элементы для Basic
+
 const inputBasic = document.getElementById('basic-count-input');
 const btnMinusBasic = document.getElementById('basic-minus');
 const btnPlusBasic = document.getElementById('basic-plus');
@@ -11,13 +12,14 @@ btnPlusBasic.onclick = function() {
 // Логика для Минуса
 btnMinusBasic.onclick = function() {
     let currentValue = parseInt(inputBasic.value);
-    if (currentValue > 1) { // Не даем уйти меньше 1
+    if (currentValue > 1) {
         inputBasic.value = currentValue - 1;
     }
 };
 
 
 // Находим элементы для Senior
+
 const inputSenior = document.getElementById('senior-count-input');
 const btnMinusSenior = document.getElementById('senior-minus');
 const btnPlusSenior = document.getElementById('senior-plus');
@@ -30,28 +32,23 @@ btnPlusSenior.onclick = function() {
 // Логика для Минуса
 btnMinusSenior.onclick = function() {
     let currentValue = parseInt(inputSenior.value);
-    if (currentValue > 1) { // Не даем уйти меньше 1
+    if (currentValue > 1) {
         inputSenior.value = currentValue - 1;
     }
 };
 
 
-/* // Подсчет общей стоимости
+// Настройка слайдера в разделе Explore picture
 
-document.getElementById('btnProduct').onclick = function() {
-    calcCost();
-};
+const slider = document.querySelector('.slider-input');
+const beforeImage = document.querySelector('.image-before');
+const handle = document.querySelector('.handle');
 
-function calcCost() {
-    var sP = document.getElementById('selectProduct').value;
-    var qP = document.getElementById('quantityProduct').value;
-    var total = sP * qP;
-    document.getElementById('priceProduct').innerHTML = total;
-    document.getElementById('infoProduct').style.display = 'block';
+slider.addEventListener('input', (e) => {
+  let value = e.target.value;
 
-    if (sP == 0 || qP == 0) {
-        document.getElementById('infoProduct').style.display = 'none';
-        alert('Укажите значения');
-        return;
-    }
-} */
+  // Меняем ширину верхнего фото
+  beforeImage.style.width = `${value}%`;
+  // Сдвигаем линию с кнопкой
+  handle.style.left = `${value}%`;
+});
